c2b349bc7bc4c6a8abcc7420dee9b08d3fcf2acc
Lazariuc Maxim <lazariucmaxim@yandex.ru>
2025-07-22 21:40:50 +0300

    Dynamic created container

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,13 @@
 import goblin from "../images/goblin.png";
 
 document.addEventListener("DOMContentLoaded", () => {
+  const container = document.querySelector(".game-container");
+  for (let i=0; i<16; i++) {
+    const cell = document.createElement("div");
+    cell.className = "cell";
+    container.append(cell);
+  }
+
   const cells = document.querySelectorAll(".cell");
   const enemy = document.createElement("img");
   enemy.setAttribute("src", goblin);
